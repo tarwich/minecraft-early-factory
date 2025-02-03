befc91d2635ab88366c716da363bd03b554cdbaf
Samuel Dillow <tarwich@gmail.com>
2025-02-02 19:04:01 -0700

    Facing is working correctly.

--- src/main/java/com/early_factory/block/BreakerBlock.java
+++ src/main/java/com/early_factory/block/BreakerBlock.java
@@ -1,14 +1,16 @@
 package com.early_factory.block;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 
-public class BreakerBlock extends Block {
+public class BreakerBlock extends DirectionalBlock {
   public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.values());
 
   public BreakerBlock() {
@@ -23,6 +25,14 @@ public class BreakerBlock extends Block {
 
   @Override
   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+    super.createBlockStateDefinition(builder);
     builder.add(FACING);
+  }
+
+  @Override
+  public BlockState getStateForPlacement(BlockPlaceContext context) {
+    // Get the opposite of the direction the player is looking at
+    // This makes the block face away from the player
+    return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection());
   }
 }
