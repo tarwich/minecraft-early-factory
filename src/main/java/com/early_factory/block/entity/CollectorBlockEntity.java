599e15dfd70ac2a5d0f35b3b8daf8d03ede053ef
Samuel Dillow <tarwich@gmail.com>
2025-02-04 17:16:57 -0700

    Give the player a chance to pick up items

--- src/main/java/com/early_factory/block/entity/CollectorBlockEntity.java
+++ src/main/java/com/early_factory/block/entity/CollectorBlockEntity.java
@@ -8,11 +8,16 @@ import com.early_factory.block.CollectorBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.IItemHandler;
 
@@ -34,15 +39,18 @@ public class CollectorBlockEntity extends BlockEntity {
         pos.getX() + (COLLECTION_RANGE + 1), pos.getY() + 2, pos.getZ() + (COLLECTION_RANGE + 1));
 
     // Get all items in range
-    List<ItemEntity> items = level.getEntitiesOfClass(
-        ItemEntity.class,
-        collectionBox);
+    List<ItemEntity> items = level.getEntitiesOfClass(ItemEntity.class, collectionBox);
 
     // Process each item
     for (ItemEntity item : items) {
       // Skip if another collector is closer to this item
       AABB itemBox = item.getBoundingBox();
       if (CollectorBlock.shouldDeferToCloserCollector(level, pos, itemBox)) {
+        continue;
+      }
+
+      // Skip if item should not be collected due to nearby player
+      if (!blockEntity.shouldCollectItem(level, pos, item)) {
         continue;
       }
 
@@ -93,5 +101,41 @@ public class CollectorBlockEntity extends BlockEntity {
             dz / distance * speed);
       }
     }
+  }
+
+  private boolean shouldCollectItem(Level level, BlockPos pos, ItemEntity itemEntity) {
+    // Check for players within 2 blocks of the item
+    List<Player> nearbyPlayers = level.getEntitiesOfClass(
+        Player.class,
+        new AABB(
+            itemEntity.getX() - 2,
+            itemEntity.getY() - 2,
+            itemEntity.getZ() - 2,
+            itemEntity.getX() + 2,
+            itemEntity.getY() + 2,
+            itemEntity.getZ() + 2),
+        player -> true);
+
+    // If there are nearby players, check if they have a clear path to the item
+    for (Player player : nearbyPlayers) {
+      // Create a ray from player eyes to item center
+      Vec3 playerEyes = player.getEyePosition();
+      Vec3 itemPos = itemEntity.position();
+
+      // Check for blocks between player and item
+      BlockHitResult hitResult = level.clip(new ClipContext(
+          playerEyes,
+          itemPos,
+          ClipContext.Block.COLLIDER,
+          ClipContext.Fluid.NONE,
+          player));
+
+      // If the ray doesn't hit any blocks, don't collect
+      if (hitResult.getType() == HitResult.Type.MISS) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
